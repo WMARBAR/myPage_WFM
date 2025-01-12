ef0b29db9fa861d76dc91ad4f9eccfe7569089e4
wmarbar <wmartinez0.01666@gmail.com>
2025-01-12 06:59:03 -0500

    se estructura la pagina

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/historias')
+def historias():
+    return render_template('historias.html')
+
+@app.route('/H_ElDiaQueElSol')
+def H_ElDiaQueElSol():
+    return render_template('H_ElDiaQueElSol.html')  # Archivo H_ElDiaQueElSol.html
+
 if __name__ == '__main__':
     app.run(debug=True)
