c789d19a72b4a11ea29110e5724d477823293681
wmarbar <wmartinez0.01666@gmail.com>
2025-01-13 19:46:06 -0500

    subi la seccion de reseñas

--- app.py
+++ app.py
@@ -14,6 +14,15 @@ def historias():
 def H_ElDiaQueElSol():
     return render_template('H_ElDiaQueElSol.html')  # Archivo H_ElDiaQueElSol.html
 
+@app.route('/resenas')
+def resenas():
+    return render_template('resenas.html')  # Archivo H_ElDiaQueElSol.html
+
+
+@app.route('/rese_tfundacion_asimov')
+def rese_tfundacion_asimov():
+    return render_template('rese_tfundacion_asimov.html')  # Archivo H_ElDiaQueElSol.html
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Usa el puerto de Render o 5000 por defecto
     app.run(debug=True, host='0.0.0.0', port=port)  # Cambia host a '0.0.0.0'
